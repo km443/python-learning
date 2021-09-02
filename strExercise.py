a92f2e7770e13180925396a23733883d3d8a8049
Rony kamruzzaman <kzamanbol@gmail.com>
2021-09-02 11:36:03 +0100

    Update strExercise.py

--- strExercise.py
+++ strExercise.py
@@ -1,4 +1,4 @@
-x=10 //declare a veriable
-y="10" //declare a string type
-z=x+y  //declare another veriable which add x and y
-print(z) //print z
+x=10 #declare a veriable
+y="10" #declare a string type
+z=x+y  #declare another veriable which add x and y
+print(z) #print z
